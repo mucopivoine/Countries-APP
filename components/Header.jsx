c71a96500cb36cbff764da6f93514c8fd0fb4d32
mucopivoine <pivotmurerwa1@gmail.com>
2024-04-03 12:40:13 +0200

    page app

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <li><a href="https://logoipsum.svg" className="text-blue-500">
         <img  className="" src={logoipsum}></img></a></li>
       <li className="text-white md:hidden" id="menu"><a href="/">Countries</a></li>
-      <button id="menu-toggle" class="lg:hidden text-white focus:outline-none">
+      <button id="menu-toggle" className="md:hidden text-white focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                         </svg>
